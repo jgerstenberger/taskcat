01eb1765b7871e666db55d72048041a15e662de1
Jason Gerstenberger <jason@gerstenberger.name>
2013-10-01 23:22:03 -0400

    Add additional task / daily task functionality

---
+++ grails-app/services/taskcat/TaskService.groovy
@@ -0,0 +1,34 @@
+package taskcat
+
+import org.joda.time.LocalDate
+import taskcat.TaskStatus;
+
+class TaskService {
+
+    def currentTasksForUser(User theUser) {
+		def tasks = Task.findAll {
+			user == theUser &&
+			status == TaskStatus.NOT_DONE
+		}
+		def dailyTasks = theUser.dailyTasks
+		
+		tasks.addAll(dailyTasks.findAll {
+			!it.instancesThru
+		}.collect{
+			new Task(user: theUser, dailyTask: it, dueDate: new LocalDate(),
+				description: it.description)
+		})
+		
+		tasks.addAll(dailyTasks.findAll {
+			it.instancesThru != null && it.instancesThru < new LocalDate()
+		}.collect { dt ->
+			(dt.instancesThru.plusDays(1)..new LocalDate()).collect() { date ->
+				new Task(user: theUser, dailyTask: dt, dueDate: date,
+					description: dt.description)
+			}
+		}.flatten())
+		
+		println tasks
+		tasks
+    }
+}
