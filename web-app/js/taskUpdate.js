64f330d33ebea433bdad10e27185b47cd9a00fbb
Jason Gerstenberger <jason@gerstenberger.name>
2014-04-17 22:05:57 -0400

    Allow update actions in category view

--- web-app/js/taskUpdate.js
+++ web-app/js/taskUpdate.js
@@ -10,15 +10,27 @@ function bindUpdateTaskButtons() {
 		$.post($('head base').attr('href') + '/task/updateStatus', 
 				{id: b.data('task-id'), dailyTaskId: b.data('daily-task-id'),
 					dueDate: b.data('due-date'), status: b.data('status'), userId: userId}, function() {
-			$.get($('head base').attr('href') + '/task?status=NOT_DONE&userId=' + userId, function(data){
-				$("#currentTasks").replaceWith(data);				
-				bindUpdateTaskButtons();
-				initSparklines();
-			});
-			$.get($('head base').attr('href') + '/task?status=DONE&userId=' + userId, function(data){
-				$("#completedTasks").replaceWith(data);				
-				initSparklines();
-			});
+			if ($("#currentTasks").length > 0) {
+				$.get($('head base').attr('href') + '/task?status=NOT_DONE&userId=' + userId, function(data){
+					$("#currentTasks").replaceWith(data);				
+					bindUpdateTaskButtons();
+					initSparklines();
+				});
+			}
+			if ($("#currentTasks").length > 0) {
+				$.get($('head base').attr('href') + '/task?status=DONE&userId=' + userId, function(data){
+					$("#completedTasks").replaceWith(data);				
+					initSparklines();
+				});
+			}
+			if ($("#categoryTasks").length > 0) {
+				$.get($('head base').attr('href') + '/task/indexCategory?userId=' + userId + '&categoryId=' + categoryId, 
+					function(data){
+						$("#categoryTasks").replaceWith(data);				
+						bindUpdateTaskButtons();
+						initSparklines();
+					});
+			}			
 		});
 	});	
 }
