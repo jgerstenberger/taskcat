bf19e2c1bdb2e9955521545b3368100b803ece6c
Jason Gerstenberger <jason@gerstenberger.name>
2014-04-26 21:56:58 -0400

    Cleanup

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,3 +1,4 @@
+import org.springframework.security.openid.OpenIDAuthenticationFilter
 import taskcat.Role
 import taskcat.User
 import taskcat.UserRole
@@ -5,17 +6,6 @@ import taskcat.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-      String password = 'password'
-
-      def roleAdmin = new Role(authority: 'ROLE_ADMIN').save()
-      def roleUser = new Role(authority: 'ROLE_USER').save()
-
-//      def user = new User(username: 'foo@bar.com', password: password, enabled: true).save()
-//      def admin = new User(username: 'admin', password: password, enabled: true).save()
-
-//      UserRole.create user, roleUser
-//      UserRole.create admin, roleUser
-//      UserRole.create admin, roleAdmin, true
     }
     def destroy = {
     }
