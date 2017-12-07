53c2986060cf6a92c020666cbf04b75459dc3728
Jason Gerstenberger <jason@gerstenberger.name>
2017-12-06 21:40:17 -0500

    upgrade to grails 2.5.6

--- grails2/grails-app/conf/BuildConfig.groovy
+++ grails2/grails-app/conf/BuildConfig.groovy
@@ -61,25 +61,25 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.42"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.1"
-        compile ':cache:1.1.1'
+        compile ":scaffolding:2.1.2"
+        compile ':cache:1.1.8'
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.3" // or ":hibernate4:4.1.11.2"
         runtime ":database-migration:1.3.8"
-        runtime ":jquery:1.10.2"
-        runtime ":resources:1.2.1"
+        runtime ":jquery:1.11.1"
+        runtime ":resources:1.2.14"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
 
-		runtime ':twitter-bootstrap:3.1.1'
-		compile ":joda-time:1.4"
+		runtime ':twitter-bootstrap:3.3.4'
+		compile ":joda-time:1.5"
 		compile ":bootstrap-datepicker:0.11"
 		compile ":quartz:1.0.1"
-		compile ":mail:1.0.1"
-		compile ":searchable:0.6.6"
+		compile ":mail:1.0.7"
+		compile ":searchable:0.6.9"
 		compile ":newrelic:1.0-2.18.0"
 		compile ":fullcalendar:1.6.4"
 		compile ":jquery-sparklines:2.1.2"
